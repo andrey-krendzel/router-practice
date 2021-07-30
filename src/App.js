0b1599aa65342929e56f8b916046a7764e0870d4
unknown <andreykrendzel@hotmail.com>
2021-07-30 21:02:33 +0100

    fridays updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import {
   BrowserRouter as Router,
-  Switch, Route, Link
+  Switch, Route, Link,
+  useParams
 } from "react-router-dom"
+
+const Note = ({ notes }) => {
+  const id = useParams().id
+  const note = notes.find(n => n.id === Number(id)) 
+  return (
+    <div>
+      <h2>{note.content}</h2>
+      <div>{note.user}</div>
+      <div><strong>{note.important ? 'important' : ''}</strong></div>
+    </div>
+  )
+}
+
+const Notes = ({notes}) => (
+  <div>
+    <h2>Notes</h2>
+    <ul>
+      {notes.map(note =>
+        <li key={note.id}>
+          <Link to={`/notes/${note.id}`}>{note.content}</Link>
+        </li>
+      )}
+    </ul>
+  </div>
+)
 
 const App = () => {
 
@@ -18,6 +44,9 @@ const App = () => {
       </div>
 
       <Switch>
+      <Route path="/notes/:id">
+        <Note notes={notes} />
+      </Route>
         <Route path="/notes">
           <Notes />
         </Route>
---
+++ src/Notes.js
@@ -0,0 +1,12 @@
+const Notes = ({notes}) => (
+    <div>
+      <h2>Notes</h2>
+      <ul>
+        {notes.map(note =>
+          <li key={note.id}>
+            <Link to={`/notes/${note.id}`}>{note.content}</Link>
+          </li>
+        )}
+      </ul>
+    </div>
+  )
